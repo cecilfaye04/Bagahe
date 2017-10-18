34b75fa093db4aae0a9817ea63714a7ff6651b67
cecil.clemente <cecil.clemente@pointwest.com.ph>
2017-10-18 18:36:58 +0800

    Added SQLite packages

---
+++ Bagahe/Bagahe/Bagahe/Views/VerifyView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Bagahe.Views
+{
+    public partial class VerifyView : ContentPage
+    {
+        public VerifyView()
+        {
+            InitializeComponent();
+
+            //    if (EnableBackButtonOverride)
+            //    {
+            //        CustomBackButtonAction = async () =>
+            //        {
+            //            var result = await this.DisplayAlert(null,
+            //                "Hey wait now! are you sure " +
+            //                "you want to go back?",
+            //                "Yes go back", "Nope");
+
+            //            if (result)
+            //            {
+            //                await Navigation.PopAsync(true);
+            //            }
+            //        };
+            //    }
+        }
+    }
+}
